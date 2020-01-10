989b57e272d5aea6b6eecf0f15477af183c9df56
Jean Felisme <jeazy@bizdotsmedia.com>
2020-01-10 15:49:58 -0500

    updated routes

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let Job = require('../models/Job');
 
+// Get all Jobs
 router.get('/', (req, res) => {
     Job.find()
         .then(jobs => res.json(jobs))
         .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
+// Add new Job
 router.post('/add', (req, res) => {
     const companyName = req.body.companyName;
     const positionTitle = req.body.positionTitle;
@@ -26,6 +28,38 @@ router.post('/add', (req, res) => {
 
     newJob.save()
         .then(() => res.json('Job added!'))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Get Single Job
+router.get('/:id', (req, res) => {
+    Job.findById(req.params.id)
+        .then(jobs => res.json(jobs))
+        .catch(err => res.status(400).json(`Error: ${err}`))
+});
+
+// Update Single Job
+router.put('/update/:id', (req, res) => {
+    Job.findById(req.params.id)
+        .then(jobs => {
+            jobs.companyName = req.body.companyName;
+            jobs.positionTitle = req.body.positionTitle;
+            jobs.submissionType = req.body.submissionType;
+            jobs.sentDocuments = req.body.sentDocuments;
+            jobs.dateApplied = Date.parse(req.body.dateApplied);
+            jobs.user = req.body._id;
+
+            jobs.save()
+                .then(() => res.json('Job updated!'))
+                .catch(err => res.status(400).json(`Error: ${err}`));
+        })
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Delete Single Job
+router.delete('/:id', (req, res) => {
+    Job.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Job Deleted!'))
         .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/User');
 
+// Get all Users
 router.get('/', (req, res) => {
     User.find()
         .then(users => res.json(users))
@@ -8,6 +9,7 @@ router.get('/', (req, res) => {
 
 });
 
+// Add New User
 router.post('/add', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -19,6 +21,34 @@ router.post('/add', (req, res) => {
 
     newUser.save()
         .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Get Single User
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Update Single User
+// router.put('/update/:id', (req, res) => {
+//     User.findById(req.params.id)
+//         .then(users => {
+//             users.email = req.body.email;
+//             users.password = req.body.password;
+
+//             users.save()
+//                 .then(() => res.json('User Updated!'))
+//                 .catch(err => res.status(400).json(`Error: ${err}`));
+//         })
+//         .catch(err => res.status(400).json(`Error: ${err}`));
+// });
+
+// Delete Single User
+router.delete('/:id', (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User Deleted!'))
         .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
